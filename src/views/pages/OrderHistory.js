ef9abd96133c77022aecb8ccdeea7dbc1dd85cfc
hello-anqi <18825150416@163.com>
2025-12-05 15:50:22 -0800

    Update OrderHistory.js

--- src/views/pages/OrderHistory.js
+++ src/views/pages/OrderHistory.js
@@ -30,7 +30,7 @@ let OrderHistory = {
         orderHistory.forEach((order, key) => {
             view += `
                 <article class="orderItem">
-                    <h3>${order.getOrderDate()}</h3>
+                    <h3>${i18n.formatDate(new Date(order.getOrderDate()))}</h3>
                     <h3>${order.orderNumber}</h3>
                     <div class="gridPrice">
                         ${i18n.formatCurrency(order.total, "w")}
